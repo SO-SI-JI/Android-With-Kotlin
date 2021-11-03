fa8e35c668399c88753f55ec8a14c610ba8211da
유지상 <jidbyoo@gmail.com>
2021-11-03 12:34:25 +0900

    RecyclerView 추가
    1. Crime 객체들을 가진 List를 참조하는 ViewModel 추가
    2. RecyclerView의 item view 및 ViewHolder 생성 @elis0m

--- CriminalIntent/app/src/main/java/com/example/criminalintent/MainActivity.kt
+++ CriminalIntent/app/src/main/java/com/example/criminalintent/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
         val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
 
         if (currentFragment == null){
-            val fragment = CrimeFragment()
+            val fragment = CrimeListFragment.newInstance()
             
             //새로운 프래그먼트 트랜잭션 인스턴스를 생성하고, 이 인스턴스에 add() 오퍼레이션을 포함시킨 후 커밋해라
             supportFragmentManager.beginTransaction().add(R.id.fragment_container, fragment).commit()
