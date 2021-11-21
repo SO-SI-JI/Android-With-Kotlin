ab40be7f1e6d877361ae5263374e15d160ba7674
유지상 <jidbyoo@gmail.com>
2021-11-21 14:39:11 +0900

    음원 로드

--- BeatBox/app/src/main/java/com/bignerdranch/android/beatbox/BeatBox.kt
+++ BeatBox/app/src/main/java/com/bignerdranch/android/beatbox/BeatBox.kt
@@ -1,8 +1,10 @@
 package com.bignerdranch.android.beatbox
 
+import android.content.res.AssetFileDescriptor
 import android.content.res.AssetManager
 import android.media.SoundPool
 import android.util.Log
+import java.io.IOException
 import java.lang.Exception
 
 private const val TAG = "BeatBox"
@@ -39,9 +41,23 @@ class BeatBox(private val assets: AssetManager) {
         soundNames.forEach { filename ->
             val assetPath = "$SOUND_FOLDER/$filename"
             val sound = Sound(assetPath)
-            sounds.add(sound)
+            try{
+                load(sound)
+                sounds.add(sound)
+            } catch (ioe: IOException){
+                Log.e(TAG, "Could not load sound $filename", ioe)
+            }
         }
 
         return sounds
+    }
+    
+    private fun load(sound: Sound){
+        val afd: AssetFileDescriptor = assets.openFd(sound.assetPath)
+        
+        // 나중에 재생할 음원 파일을 SoundPool에 로드
+        // 음원을 유지하고 다시 재생하기 위해 정수 ID를 반환
+        val soundId = soundPool.load(afd, 1)
+        sound.soundId = soundId
     }
 }
