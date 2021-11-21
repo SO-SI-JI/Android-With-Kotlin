64ecc46870923af5cba42e7a39252477a0db015a
유지상 <jidbyoo@gmail.com>
2021-11-21 15:06:54 +0900

    테스트 대상 설정

--- BeatBox/app/src/test/java/com/bignerdranch/android/beatbox/SoundViewModelTest.kt
+++ BeatBox/app/src/test/java/com/bignerdranch/android/beatbox/SoundViewModelTest.kt
@@ -3,10 +3,19 @@ package com.bignerdranch.android.beatbox
 import org.junit.Assert.*
 
 import org.junit.Before
+import org.junit.Test
 
 class SoundViewModelTest {
 
+    private lateinit var sound: Sound
+    private lateinit var subject: SoundViewModel
+
     @Before
     fun setUp() {
+        sound = Sound("assetPath")
+        
+        // 테스트 대상이 되는 객체를 Subject로 칭함
+        subject = SoundViewModel()
+        subject.sound = sound
     }
 }
