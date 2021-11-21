fda9a328553c16cd30dc3b8764b71224fc22d68a
유지상 <jidbyoo@gmail.com>
2021-11-21 14:28:17 +0900

    데이터 객체 값 변경 감지

--- BeatBox/app/src/main/java/com/bignerdranch/android/beatbox/SoundViewModel.kt
+++ BeatBox/app/src/main/java/com/bignerdranch/android/beatbox/SoundViewModel.kt
@@ -1,14 +1,21 @@
 package com.bignerdranch.android.beatbox
 
+import androidx.databinding.BaseObservable
+import androidx.databinding.Bindable
+
 // 뷰모델
-class SoundViewModel {
+class SoundViewModel : BaseObservable(){
 
     var sound: Sound? = null
         set(sound){
             field = sound
+
+            // Sound의 모든 바인딩 속성값이 변경되었음을 ListItemSoundBinding에 알린다
+            notifyChange()
         }
 
     // 버튼 제목을 갖는 속성
+    @get:Bindable
     val title: String?
         get() = sound?.name
 }
