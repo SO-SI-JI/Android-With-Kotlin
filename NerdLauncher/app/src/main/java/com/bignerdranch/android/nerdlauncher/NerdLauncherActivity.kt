248bbeaa0020171f8680b27c5df1342bd9f8d5de
유지상 <jidbyoo@gmail.com>
2021-11-25 15:48:35 +0900

    프로젝트 설정 변경

--- NerdLauncher/app/src/main/java/com/bignerdranch/android/nerdlauncher/NerdLauncherActivity.kt
+++ NerdLauncher/app/src/main/java/com/bignerdranch/android/nerdlauncher/NerdLauncherActivity.kt
@@ -3,9 +3,9 @@ package com.bignerdranch.android.nerdlauncher
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class NerdLauncherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_nerd_launcher)
     }
 }
