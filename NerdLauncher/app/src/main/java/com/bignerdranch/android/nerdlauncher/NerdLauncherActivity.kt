4bd0e066c4577fd30c0e30a750c3bb46202eba92
유지상 <jidbyoo@gmail.com>
2021-11-25 16:07:50 +0900

    액티비티 리스트 표시
    1. 암시적 인텐트로 액티비티 리스트 가져오기
    2. RecyclerView로 액티비티 리스트 표시

--- NerdLauncher/app/src/main/java/com/bignerdranch/android/nerdlauncher/NerdLauncherActivity.kt
+++ NerdLauncher/app/src/main/java/com/bignerdranch/android/nerdlauncher/NerdLauncherActivity.kt
@@ -1,11 +1,85 @@
 package com.bignerdranch.android.nerdlauncher
 
+import android.content.Intent
+import android.content.pm.ResolveInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+private const val TAG = "NerdLauncherActivity"
 
 class NerdLauncherActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nerd_launcher)
+
+
+        recyclerView = findViewById(R.id.app_recycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        setupAdapter()
+    }
+
+    private fun setupAdapter(){
+        
+        // 암시적 인텐트 생성
+        val startupIntent = Intent(Intent.ACTION_MAIN).apply {
+            addCategory(Intent.CATEGORY_LAUNCHER)
+        }
+        
+        // 인텐트와 일치하는 액티비티들의 리스트
+        val activities = packageManager.queryIntentActivities(startupIntent, 0)
+
+        
+        // 알파벳 순으로 정렬
+        activities.sortWith(Comparator { a, b ->
+            String.CASE_INSENSITIVE_ORDER.compare(
+                a.loadLabel(packageManager).toString(),
+                b.loadLabel(packageManager).toString()
+            )
+        })
+
+        Log.i(TAG, "Found ${activities.size} activities")
+        
+        // ActivityAdapter의 인스턴스를 생성해서 RecyclerView의 어댑터로 설정
+        recyclerView.adapter = ActivityAdapter(activities)
+    }
+
+    private class ActivityHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        private val nameTextView = itemView as TextView
+        private lateinit var resolveInfo: ResolveInfo
+
+        fun bindActivity(resolveInfo: ResolveInfo){
+            this.resolveInfo = resolveInfo
+            val packageManager = itemView.context.packageManager
+            val appName = resolveInfo.loadLabel(packageManager).toString()
+            nameTextView.text =appName
+        }
+    }
+
+    private class ActivityAdapter(val activities: List<ResolveInfo>) : RecyclerView.Adapter<ActivityHolder>(){
+        override fun onCreateViewHolder(container: ViewGroup, viewType: Int): ActivityHolder {
+            val layoutInflater = LayoutInflater.from(container.context)
+            val view = layoutInflater.inflate(android.R.layout.simple_list_item_1, container, false)
+            return ActivityHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: ActivityHolder, position: Int) {
+            val resolveInfo = activities[position]
+            holder.bindActivity(resolveInfo)
+        }
+
+        override fun getItemCount(): Int{
+            return activities.size
+        }
     }
 }
