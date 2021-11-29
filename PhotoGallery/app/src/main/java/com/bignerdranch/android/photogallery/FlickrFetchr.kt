183017aa46710e6f0eac7591be7ab7c0f24e0b7a
유지상 <jidbyoo@gmail.com>
2021-11-29 17:24:25 +0900

    네트워크 관련 코드 리팩토링

---
+++ PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/FlickrFetchr.kt
@@ -0,0 +1,50 @@
+package com.bignerdranch.android.photogallery
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.bignerdranch.android.photogallery.api.FlickrApi
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+
+private const val TAG = "FlickrFetchr"
+
+class FlickrFetchr {
+    private val flickrApi: FlickrApi
+
+    init {
+
+        // Retrofit이 응답을 문자열로 역직렬화하도록 Retrofit 객체를 생성할 때 변환기를 지정
+        // 플리커 응답을 String 객체로 역직렬화
+        val retrofit: Retrofit = Retrofit.Builder()
+            .baseUrl("https://www.flickr.com/")
+            .addConverterFactory(ScalarsConverterFactory.create())
+            .build()
+
+        flickrApi = retrofit.create(FlickrApi::class.java)
+    }
+
+    fun fetchContents(): LiveData<String> {
+        val responseLiveData: MutableLiveData<String> = MutableLiveData()
+        val flickrRequest: Call<String> = flickrApi.fetchContents()
+
+        flickrRequest.enqueue(object : Callback<String> {
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                Log.e(TAG, "Failed to fetch photos", t)
+            }
+
+            override fun onResponse(
+                call: Call<String>,
+                response: Response<String>
+            ) {
+                Log.d(TAG, "Response received")
+                responseLiveData.value = response.body()
+            }
+        })
+
+        return responseLiveData
+    }
+}
--- PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
+++ PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bignerdranch.android.photogallery.api.FlickrApi
@@ -23,16 +25,7 @@ class PhotoGalleryFragment : Fragment(){
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
 
-        // Retrofit이 응답을 문자열로 역직렬화하도록 Retrofit 객체를 생성할 때 변환기를 지정
-        // 플리커 응답을 String 객체로 역직렬화
-        val retrofit: Retrofit = Retrofit.Builder()
-            .baseUrl("https://www.flickr.com/")
-            .addConverterFactory(ScalarsConverterFactory.create())
-            .build()
-
-        val flickrApi : FlickrApi = retrofit.create(FlickrApi::class.java)
-
-        val flickrHomePageRequest: Call<String> = flickrApi.fetchContents()
+/*        val flickrHomePageRequest: Call<String> = flickrApi.fetchContents()
 
         // Call 객체가 나타내는 웹 요청을 백그라운드 스레드에서 실행
         // 큐로 관리하며, Retrofit은 큐가 비워질 때까지 하나씩 요청을 처리한다.
@@ -51,7 +44,17 @@ class PhotoGalleryFragment : Fragment(){
             
             // 안드로이드는 main 스레드에서의 네트워크 작업 수행을 허용하지 않음
             // NetworkOnMainThreadException
-        })
+        })*/
+        
+        // FlickrFetchr는 PhotoGallery 앱의 네트워크 관련 코드를 가짐
+        // fetchContents는 네트워크 요청을 큐에 넣고 응답 결과를 LiveData로 반환
+        val flickrLiveData: LiveData<String> = FlickrFetchr().fetchContents()
+        flickrLiveData.observe(
+            this,
+            Observer { responseString ->
+                Log.d(TAG, "Response Received: $responseString")
+            }
+        )
     }
 
     override fun onCreateView(
