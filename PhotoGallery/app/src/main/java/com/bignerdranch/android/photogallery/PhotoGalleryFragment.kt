b6f300b7227a773442d215285f62474b16d6267c
유지상 <jidbyoo@gmail.com>
2021-11-29 16:01:18 +0900

    비동기로 웹 요청 실행

--- PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
+++ PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
@@ -1,6 +1,7 @@
 package com.bignerdranch.android.photogallery
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,8 +10,12 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bignerdranch.android.photogallery.api.FlickrApi
 import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.scalars.ScalarsConverterFactory
+
+private const val TAG = "PhotoGalleryFragment"
 
 class PhotoGalleryFragment : Fragment(){
     private lateinit var photoRecyclerView: RecyclerView
@@ -28,6 +33,25 @@ class PhotoGalleryFragment : Fragment(){
         val flickrApi : FlickrApi = retrofit.create(FlickrApi::class.java)
 
         val flickrHomePageRequest: Call<String> = flickrApi.fetchContents()
+
+        // Call 객체가 나타내는 웹 요청을 백그라운드 스레드에서 실행
+        // 큐로 관리하며, Retrofit은 큐가 비워질 때까지 하나씩 요청을 처리한다.
+        flickrHomePageRequest.enqueue(object: Callback<String> {
+            override fun onFailure(call: Call<String>, t: Throwable){
+                Log.e(TAG, "Failed to fetch Photos", t)
+            }
+
+            // Response 객체는 자신의 몸체에 결과 콘텐츠를 포함
+            override fun onResponse(
+                call: Call<String>,
+                response: Response<String>
+            ){
+                Log.d(TAG, "Response received: ${response.body()}")
+            }
+            
+            // 안드로이드는 main 스레드에서의 네트워크 작업 수행을 허용하지 않음
+            // NetworkOnMainThreadException
+        })
     }
 
     override fun onCreateView(
