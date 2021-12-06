d675195d2bbdb99f34c0267fc9b8fe921a214df5
유지상 <jidbyoo@gmail.com>
2021-12-06 13:02:50 +0900

    RecylclerView에 응답 결과 보여주기

--- PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
+++ PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
@@ -5,9 +5,11 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bignerdranch.android.photogallery.api.FlickrApi
@@ -20,6 +22,7 @@ import retrofit2.converter.scalars.ScalarsConverterFactory
 private const val TAG = "PhotoGalleryFragment"
 
 class PhotoGalleryFragment : Fragment(){
+    private lateinit var photoGalleryViewModel: PhotoGalleryViewModel
     private lateinit var photoRecyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?){
@@ -46,7 +49,7 @@ class PhotoGalleryFragment : Fragment(){
             // NetworkOnMainThreadException
         })*/
         
-        // FlickrFetchr는 PhotoGallery 앱의 네트워크 관련 코드를 가짐
+/*        // FlickrFetchr는 PhotoGallery 앱의 네트워크 관련 코드를 가짐
         // fetchContents는 네트워크 요청을 큐에 넣고 응답 결과를 LiveData로 반환
         val flickrLiveData: LiveData<List<GalleryItem>> = FlickrFetchr().fetchPhotos()
         flickrLiveData.observe(
@@ -54,7 +57,8 @@ class PhotoGalleryFragment : Fragment(){
             Observer { galleryItems ->
                 Log.d(TAG, "Response Received: $galleryItems")
             }
-        )
+        )*/
+        photoGalleryViewModel = ViewModelProvider(this).get(PhotoGalleryViewModel::class.java)
     }
 
     override fun onCreateView(
@@ -76,5 +80,34 @@ class PhotoGalleryFragment : Fragment(){
 
     companion object{
         fun newInstance() = PhotoGalleryFragment()
+    }
+
+    private class PhotoHolder(itemTextView: TextView): RecyclerView.ViewHolder(itemTextView){
+        val bindTitle: (CharSequence) -> Unit = itemTextView::setText
+    }
+
+    private class PhotoAdapter(private val galleryItems: List<GalleryItem>): RecyclerView.Adapter<PhotoHolder>(){
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoHolder {
+            val textView = TextView(parent.context)
+            return PhotoHolder(textView)
+        }
+
+        override fun getItemCount(): Int = galleryItems.size
+
+        override fun onBindViewHolder(holder: PhotoHolder, position: Int) {
+            val galleryItem = galleryItems[position]
+            holder.bindTitle(galleryItem.title)
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        photoGalleryViewModel.galleryItemLiveData.observe(
+            viewLifecycleOwner,
+            Observer { galleryItems ->
+                photoRecyclerView.adapter = PhotoAdapter(galleryItems)
+            }
+        )
     }
 }
