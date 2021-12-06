774cb5da39fb8ca1898fffdfa57187aba9e5e42d
유지상 <jidbyoo@gmail.com>
2021-12-06 12:33:27 +0900

    최근 사진 가져오는 요청 정의

--- PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
+++ PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
@@ -4,8 +4,17 @@ import retrofit2.Call
 import retrofit2.http.GET
 
 interface FlickrApi {
-    @GET("/")
-    
-    // String으로 지정했으므로, HTTP 응답이 String 객체로 역직렬화
-    fun fetchContents(): Call<String>
+    /*
+        @GET("/")
+        // String으로 지정했으므로, HTTP 응답이 String 객체로 역직렬화
+        fun fetchContents(): Call<String>
+    */
+    @GET(
+        "services/rest/?method=flickr.interestingness.getList" +
+                "&api_key=7dae9721c3af0a9ccd511215078c6bc8" +
+                "&format=json" +
+                "&nojsoncallback=1" +
+                 "&extras=url_s"
+    )
+    fun fetchPhotos(): Call<String>
 }
