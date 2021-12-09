9f5c98ed490028a8615eca87e1fb57e9cac7b5bc
유지상 <jidbyoo@gmail.com>
2021-12-09 15:21:00 +0900

    메시지 전달

--- PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/ThumbnailDownloader.kt
+++ PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/ThumbnailDownloader.kt
@@ -1,15 +1,21 @@
 package com.bignerdranch.android.photogallery
 
+import android.os.Handler
 import android.os.HandlerThread
 import android.util.Log
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.OnLifecycleEvent
+import java.util.concurrent.ConcurrentHashMap
 
 private const val TAG = "ThumbnailDownloader"
+private const val MESSAGE_DOWNLOAD = 0
 
 class ThumbnailDownloader<in T> : HandlerThread(TAG), LifecycleObserver {
     private var hasQuit = false
+    private lateinit var requestHandler: Handler
+    private val requestMap = ConcurrentHashMap<T, String>()
+    private val flickrFetchr = FlickrFetchr()
 
     override fun quit(): Boolean{
         hasQuit = true
@@ -18,6 +24,10 @@ class ThumbnailDownloader<in T> : HandlerThread(TAG), LifecycleObserver {
 
     fun queueThumbnail(target: T, url: String){
         Log.i(TAG, "Got a URL: $url")
+        
+        // 메시지를 가져와서 전달하기
+        requestMap[target] = url
+        requestHandler.obtainMessage(MESSAGE_DOWNLOAD, target).sendToTarget()
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
