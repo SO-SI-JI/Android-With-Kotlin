d5dfe47caa8802847e60c27cf451cb44445a40a7
유지상 <jidbyoo@gmail.com>
2021-12-27 11:00:08 +0900

    검색 함수 추가

--- PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
+++ PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
@@ -3,6 +3,7 @@ package com.bignerdranch.android.photogallery.api
 import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 import retrofit2.http.Url
 
 interface FlickrApi {
@@ -11,15 +12,15 @@ interface FlickrApi {
         // String으로 지정했으므로, HTTP 응답이 String 객체로 역직렬화
         fun fetchContents(): Call<String>
     */
-    @GET(
-        "services/rest/?method=flickr.interestingness.getList" +
-                "&api_key=7dae9721c3af0a9ccd511215078c6bc8" +
-                "&format=json" +
-                "&nojsoncallback=1" +
-                 "&extras=url_s"
+    @GET("services/rest?method=flickr.interestingness.getList"
     )
     fun fetchPhotos(): Call<FlickrResponse>
 
     @GET
     fun fetchUrlBytes(@Url url: String): Call<ResponseBody>
+
+    
+    //파라미터 동적으로 추가
+    @GET("services/rest?method=flickr.photos.search")
+    fun searchPhotos(@Query("text") query: String): Call<FlickrResponse>
 }
