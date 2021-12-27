be591279cc71cda69d0896e0708dccc4618c214d
유지상 <jidbyoo@gmail.com>
2021-12-27 10:54:21 +0900

    Intercepter 클래스 생성

---
+++ PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/api/PhotoInterceptor.kt
@@ -0,0 +1,28 @@
+package com.bignerdranch.android.photogallery.api
+
+import okhttp3.HttpUrl
+import okhttp3.Interceptor
+import okhttp3.Request
+import okhttp3.Response
+
+private const val API_KEY = "7dae9721c3af0a9ccd511215078c6bc8"
+
+class PhotoInterceptor: Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val originalRequest: Request = chain.request()
+
+        val newUrl: HttpUrl = originalRequest.url().newBuilder()
+            .addQueryParameter("api_key", API_KEY)
+            .addQueryParameter("format", "json")
+            .addQueryParameter("nojsoncallback", "1")
+            .addQueryParameter("extras", "url_s")
+            .addQueryParameter("safesearch", "")
+            .build()
+
+        val newRequest: Request = originalRequest.newBuilder()
+            .url(newUrl)
+            .build()
+
+        return chain.proceed(newRequest)
+    }
+}
