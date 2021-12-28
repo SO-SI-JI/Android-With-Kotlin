443b1b479b2899a1db861adc0e653a53c99b2cf7
유지상 <jidbyoo@gmail.com>
2021-12-28 16:59:14 +0900

    최근 ID를 저장하거나 가져옴

--- PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/QueryPreferences.kt
+++ PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/QueryPreferences.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.preference.PreferenceManager
 
 private const val PREF_SEARCH_QUERY = "searchQuery"
+private const val PREF_LAST_RESULT_ID = "lastResultId"
 
 object QueryPreferences {
     fun getStoredQuery(context: Context): String{
@@ -13,5 +14,14 @@ object QueryPreferences {
 
     fun setStoredQuery(context: Context, query: String){
         PreferenceManager.getDefaultSharedPreferences(context).edit().putString(PREF_SEARCH_QUERY, query).apply()
+    }
+
+    // 사용자가 보았던 가장 최근 사진 ID를 저장하거나 가져옴
+    fun getLastResultId(context: Context): String{
+        return PreferenceManager.getDefaultSharedPreferences(context).getString(PREF_LAST_RESULT_ID, "")!!
+    }
+
+    fun setLastResultId(context: Context, lastResultId: String){
+        PreferenceManager.getDefaultSharedPreferences(context).edit().putString(PREF_LAST_RESULT_ID, lastResultId).apply()
     }
 }
