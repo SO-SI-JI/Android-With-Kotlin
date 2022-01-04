d322c1576c37dfd3b8f472a8ff1583ae0b989354
유지상 <jidbyoo@gmail.com>
2022-01-04 16:47:01 +0900

    브로드캐스트 인텐트 전송

--- PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/PollWorker.kt
+++ PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/PollWorker.kt
@@ -3,6 +3,7 @@ package com.bignerdranch.android.photogallery
 import android.app.Notification
 import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
@@ -51,8 +52,15 @@ class PollWorker(val context: Context, workerParams: WorkerParameters): Worker(c
 
         val notificationManager = NotificationManagerCompat.from(context)
         notificationManager.notify(0, notification)
+        
+        // 새로운 검색 결과를 게시할 준비가 되었음을 관심있는 컴포넌트에 알리는 브로드캐스트 인텐트를 전송
+        context.sendBroadcast(Intent(ACTION_SHOW_NOTIFICATION))
 
         // 작업이 성공했음
         return Result.success()
+    }
+
+    companion object {
+        const val ACTION_SHOW_NOTIFICATION = "com.bignerdranch.android.photogallery.SHOW_NOTIFICATION"
     }
 }
