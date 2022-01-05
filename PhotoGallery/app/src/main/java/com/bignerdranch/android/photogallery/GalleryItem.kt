446f20e78a0bfa53caa1a9a08d370ba8f1bfdc54
유지상 <jidbyoo@gmail.com>
2022-01-05 14:43:17 +0900

    사진 웹 페이지 관련 코드 추가
    JSON 데이터의 owner 속성값을 받기 위해 GalleryItem 클래스에 owner 속성과 사진 웹페이지의 URL을 가지는 연산 속성인 photoPageUri를 추가

--- PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/GalleryItem.kt
+++ PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/GalleryItem.kt
@@ -1,5 +1,6 @@
 package com.bignerdranch.android.photogallery
 
+import android.net.Uri
 import com.google.gson.annotations.SerializedName
 
 data class GalleryItem(
@@ -7,5 +8,15 @@ data class GalleryItem(
             var id: String = "",
 
             // JSON 객체의 필드 이름과 다르면 @SerializedName
-            @SerializedName("url_s") var url: String = ""
-)
+            @SerializedName("url_s") var url: String = "",
+            @SerializedName("owner") var owner: String = ""
+) {
+    val photoPageUri: Uri
+        get(){
+            return Uri.parse("https://www.flickr.com/photos/")
+                .buildUpon()
+                .appendPath(owner)
+                .appendPath(id)
+                .build()
+        }
+}
