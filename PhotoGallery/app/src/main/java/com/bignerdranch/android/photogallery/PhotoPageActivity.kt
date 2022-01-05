7b3e1fa5394b32fb28cc3cf157c6240c471bb8eb
유지상 <jidbyoo@gmail.com>
2022-01-05 15:07:38 +0900

    Create PhotoPageActivity.kt

---
+++ PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/PhotoPageActivity.kt
@@ -0,0 +1,30 @@
+package com.bignerdranch.android.photogallery
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PhotoPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_photo_page)
+
+        val fm = supportFragmentManager
+        val currentFragment = fm.findFragmentById(R.id.fragmentContainer)
+
+        if (currentFragment == null){
+            val fragment = PhotoPageFragment.newInstance(intent.data!!)
+            fm.beginTransaction().add(R.id.fragmentContainer, fragment).commit()
+        }
+    }
+
+    companion object {
+        fun newIntent(context: Context, photoPageUri: Uri): Intent {
+            return Intent(context, PhotoPageActivity::class.java).apply {
+                data = photoPageUri
+            }
+        }
+    }
+}
